8a4e532ab74552b47c259bd683e82fe11410ea36
Renan Nogueira <naanzitos@gmail.com>
2025-07-30 14:11:28 -0300

    fix

--- inject-store.js
+++ inject-store.js
@@ -51,7 +51,10 @@ try {
 
     // Add named export for make-in-memory-store
     if (fileName === 'make-in-memory-store.js') {
+      // Add both CommonJS and ES6 exports
       content += '\n\n// Named export for compatibility\nexports.makeInMemoryStore = exports.default;\n'
+      content += '\n// ES6 module compatibility\nmodule.exports.makeInMemoryStore = exports.default;\n'
+      content += 'module.exports.default = exports.default;\n'
     }
 
     // Copy to final destination
